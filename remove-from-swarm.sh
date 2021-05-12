304f72477b2c27455b4a468e0030bde03a5a7e14
Victor Song <vms2@rice.edu>
2021-05-12 04:59:51 -0500

    Ran shellcheck on the rest of the shell files

--- remove-from-swarm.sh
+++ remove-from-swarm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd $(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" || exit
 
 echo "Removing gpu-jupyter from docker swarm."
 docker stack rm gpu
